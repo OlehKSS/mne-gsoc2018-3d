c1bc5c6a7d8323d63b5a52b82e53a7dd52e32cc3
Oleh Kozynets <ok7mailbox@gmail.com>
2018-05-23 22:55:28 +0200

    Fixes #4. Mesh overlay with data activation.

--- mne_g3d/viz.py
+++ mne_g3d/viz.py
@@ -1,4 +1,7 @@
 import ipyvolume as ipv
+from pythreejs import BlendFactors, BlendingMode, Equations
+from pythreejs import Side, ShaderMaterial
+import matplotlib.pyplot as plt
 
 
 def plot_brain_mesh(rh_vertices=None,
@@ -7,6 +10,7 @@ def plot_brain_mesh(rh_vertices=None,
                     lh_faces=None,
                     rh_color='grey',
                     lh_color='grey',
+                    act_data=None,
                     fig_size=(500, 500),
                     azimuth=90,
                     elevation=90):
@@ -34,6 +38,8 @@ def plot_brain_mesh(rh_vertices=None,
         Color for each point/vertex/symbol of the left hemisphere,
         can be string format, examples for red:’red’, ‘#f00’, ‘#ff0000’ or
         ‘rgb(1,0,0), or rgb array of shape (N, 3). Default value is 'grey'.
+    act_data : {"lh": numpy.array, "rh": numpy.array}, optional
+        Dictionary with activation data for each hemisphere.
     fig_size : (int, int), optional
         Width and height of the figure. Default is (500, 500).
     azimuth : int, optional
@@ -58,15 +64,21 @@ def plot_brain_mesh(rh_vertices=None,
     fig = ipv.figure(width=fig_size[0], height=fig_size[1], lighting=True)
 
     if (rh_vertices is not None) and (rh_faces is not None):
-        rh_mesh = plot_hemisphere_mesh(rh_vertices, rh_faces, rh_color)
+        rh_mesh, _ = plot_hemisphere_mesh(rh_vertices,
+                                          rh_faces,
+                                          rh_color,
+                                          act_data=act_data["rh"])
 
     if (lh_vertices is not None) and (lh_faces is not None):
-        lh_mesh = plot_hemisphere_mesh(lh_vertices, lh_faces, lh_color)
+        lh_mesh, _ = plot_hemisphere_mesh(lh_vertices,
+                                          lh_faces,
+                                          lh_color,
+                                          act_data=act_data["lh"])
 
     ipv.style.box_off()
     ipv.style.axes_off()
     ipv.style.background_color('black')
-    
+
     ipv.view(azimuth, elevation)
     ipv.squarelim()
     ipv.show()
@@ -74,7 +86,7 @@ def plot_brain_mesh(rh_vertices=None,
     return fig, rh_mesh, lh_mesh
 
 
-def plot_hemisphere_mesh(vertices, faces,  color='grey'):
+def plot_hemisphere_mesh(vertices, faces,  color='grey', act_data=None):
     u"""Plot triangular format Freesurfer surface of the brain hemispheres.
 
     Parameters
@@ -87,16 +99,50 @@ def plot_hemisphere_mesh(vertices, faces,  color='grey'):
         Color for each point/vertex/symbol, can be string format, examples for
         red:’red’, ‘#f00’, ‘#ff0000’ or ‘rgb(1,0,0), or rgb array of
         shape (N, 3). Default value is 'grey'.
+    act_data : numpy.array, optional
+        Activation data for the given hemispere.
 
     Returns
     -------
     mesh_widget : ipyvolume.Mesh
         Ipyvolume object presenting the built mesh.
+    mesh_overlay : ipyvolume.Mesh
+        Ipyvolume object presenting the transparent overlay with
+        activation data, if available.
     """
+    cmap = plt.get_cmap("hot")
+
     x = vertices[:, 0]
     y = vertices[:, 1]
     z = vertices[:, 2]
 
     mesh_widget = ipv.plot_trisurf(x, y, z, triangles=faces, color=color)
 
-    return mesh_widget
+    mesh_overlay = None
+    # Add mesh overlay and plot data on top of it
+    if act_data is not None:
+        # transform into color
+        act_data -= act_data.min()
+        act_data /= act_data.max()
+        act_data[act_data > 1.] = 1.
+        act_colors = cmap(act_data.ravel())[:, :3]
+
+        mesh_overlay = ipv.plot_trisurf(x,
+                                        y,
+                                        z,
+                                        triangles=faces,
+                                        color=act_colors)
+
+        # Tranparency and color blending for the new material of the mesh
+        mat = ShaderMaterial()
+        mat.alphaTest = 0.1
+        mat.blending = BlendingMode.CustomBlending
+        mat.blendDst = BlendFactors.OneFactor
+        mat.blendEquation = Equations.AddEquation
+        mat.blendSrc = BlendFactors.SrcColorFactor
+        mat.transparent = True
+        mat.side = Side.DoubleSide
+
+        mesh_overlay.material = mat
+
+    return mesh_widget, mesh_overlay
